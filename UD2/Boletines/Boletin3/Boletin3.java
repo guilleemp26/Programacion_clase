14571b2aab661a40010548f8d51201ee14406849
guilleemp26 <guillemporta26@gmail.com>
2025-11-12 02:13:25 +0100

    Boletín 3 hasta el ejercicio 19

--- UD2/Boletines/Boletin3/Boletin3.java
+++ UD2/Boletines/Boletin3/Boletin3.java
@@ -5,9 +5,10 @@ import java.util.Scanner;
 public class Boletin3
 {
     public static Scanner sc = new Scanner(System.in);
+    public static Random random = new Random();
     public static void main(String[] args) 
     {
-        ej12();
+        ej19();
     }
     public static void ej1()
     {
@@ -20,7 +21,7 @@ public class Boletin3
             suma += i;
             if(suma >= num)
             {
-                break;
+                break; 
             }
         }   
         System.out.println(suma);
@@ -71,7 +72,6 @@ public class Boletin3
     {
         int min = 5;
         int max = 25;
-        Random random = new Random();
 
         for(int i = 0;i <= 10;i++)
         {
@@ -87,7 +87,6 @@ public class Boletin3
     {
          int min = 5;
         int max = 25;
-        Random random = new Random();
         int num = 1; // num comienza en 1 porque si comienza en 0 no entra en el for debido a que (0 % 5) = 0
 
         for(int i = 0;i <= 10 && ((num % 5) != 0);i++)
@@ -96,7 +95,7 @@ public class Boletin3
             System.out.println("Número aleatorio entre 5 y 25: " + num);
         }
     }
-    public static void ej7()//Al introducir 1 o 2 como valor de altura se rompe 
+    public static void ej7() 
     {
         System.out.print("Introduzca la cantidad de alturas que va a facilitar: ");
         int numAlturas = Integer.parseInt(sc.nextLine());
@@ -104,11 +103,14 @@ public class Boletin3
         int suma = 0;
         int promedio;
 
-        for(int i = 1;i < numAlturas; i++)
+        for(int i = 1;i <= numAlturas; i++)
         {
-            System.out.printf("Introduce la altura número %d: ", (i +1));
-            suma += numAlturas = Integer.parseInt(sc.nextLine());
+            System.out.printf("Introduce la altura número %d: ", (i));
+            suma += Integer.parseInt(sc.nextLine());
         }
+        promedio = suma / numAlturas;
+        System.out.printf("La altura promedio es %d", promedio);
+
     }
     public static void ej8()
     {
@@ -201,5 +203,216 @@ public class Boletin3
             }
             numActual++;
         }
+    }
+    public static void ej13()
+    {
+        String continuar = "si";
+        int mayor = 0;
+        int menor = 0;
+        int sumaPos = 0;
+        int sumaNeg = 0;
+        int positivos = 0;
+        int negativos = 0;
+        int impares = 0;
+        int pares = 0;
+        int contador = 0;
+        int suma = 0;
+        int media = 0;
+
+        do
+        {
+            System.out.print("Introduzca un número: ");
+            int num = Integer.parseInt(sc.nextLine());
+            System.out.print("Quieres introducir más números? si/no: ");
+            continuar = sc.nextLine();
+            contador++;
+            suma += num;
+            if(num > mayor)
+            {
+                mayor = num;
+            }
+            if(num < menor)
+            {
+                menor = num;
+            }
+            if(num > 0)
+            {
+                sumaPos += num;
+                positivos++;
+            }
+            if(num < 0)
+            {
+                sumaNeg += num;
+                negativos++;
+            }
+            if((num % 2) == 0)
+            {
+                pares++;
+            }
+            if((num % 2) != 0)
+            {
+                impares++;
+            }
+        }while(continuar.equals("si"));
+        media = suma / contador;
+
+        System.out.printf("mayor = %d%n", mayor);
+        System.out.printf("menor = %d%n", menor);
+        System.out.printf("sumaPos = %d%n", sumaPos);
+        System.out.printf("sumaNeg = %d%n", sumaNeg);
+        System.out.printf("positivos = %d%n", positivos);
+        System.out.printf("negativos = %d%n", negativos);
+        System.out.printf("impares = %d%n", impares);
+        System.out.printf("pares = %d%n", pares);
+        System.out.printf("contador = %d%n", contador);
+        System.out.printf("suma = %d%n", suma);
+        System.out.printf("media = %d%n", media);
+    }
+    public static void ej14()//Profe no se como hacerlo
+    {
+       System.out.print("Introduce un número para saber si es un \"número Armstrong: \" ");
+        int num = Integer.parseInt(sc.nextLine());
+        int aux = num;
+        int contador = 0;
+        int miles = 0;
+        int centenas = 0;
+        int decenas = 0;
+
+        for(int i = 0;num > 0;i++)
+        {
+            int digito = num % 10;
+            System.out.println(digito);
+            num = num / 10;
+            contador++;
+        }
+        System.out.println(contador);
+        System.out.println(num);
+    }
+    public static void ej15()//Ni idea profe lo siento
+    {
+
+    }
+    public static void ej16()//XD
+    {
+
+    }
+    public static void ej17()//solo funciona ocn operandos de un decimal
+    {
+        System.out.print
+        ("                    \n" + 
+         "********************\n" + 
+         "                    \n" + 
+         "    Calculadora     \n" + 
+         "                    \n" +
+         "********************\n" + 
+         "1. Sumar: +         \n" + 
+         "2. Restar: -        \n" + 
+         "3. Multiplicar: *   \n" + 
+         "4. Dividir: /       \n" + 
+         "5. Potencia: ^      \n" + 
+         "6. Módulo: %        \n" +
+         "                    \n" + 
+         "                     \n"
+        );
+
+        System.out.print("Introduzca la operación. Ejemplo 8+2: ");
+        String num = sc.nextLine();
+        char num1Char = num.charAt(0);
+        char operador = num.charAt(1);
+        char num2Char = num.charAt(2);
+        int num1 = num1Char - '0';
+        int num2 = num2Char - '0';
+
+        if(operador == '+')
+        {
+            int resultado = num1 + num2;
+            System.out.print(num1 + " + " + num2 + " = " + resultado);
+        }
+        if(operador == '-')
+        {
+            int resultado = num1 - num2;
+            System.out.print(num1 + " - " + num2 + " = " + resultado);
+        }
+        if(operador == '*')
+        {
+            int resultado = num1 * num2;
+            System.out.print(num1 + " * " + num2 + " = " + resultado);
+        }
+        if(operador == 47)
+        {
+            double resultado = num1 / num2;
+            System.out.print(num1 + " / " + num2 + " = " + resultado);
+        }
+        if(operador == '^')
+        {
+            double resultado = Math.pow(num1, num2);
+            System.out.print(num1 + " * " + num2 + " = " + resultado);
+        }
+        if(operador == '%')
+        {
+            int resultado = num1 % num2;
+            System.out.print(num1 + " % " + num2 + " = " + resultado);
+        }
+    }
+    public static void ej18()
+    {
+        System.out.print("Introduce un número: ");
+        int num = Integer.parseInt(sc.nextLine());
+        int contador = 1;
+        int columnas = 1;
+
+        for(int i = 1;contador <= num;i++)
+        {
+            for(int j = 1;j <= columnas;j++)
+            {
+                System.out.print(contador + " ");
+                contador++;
+                if(contador > num)
+                {
+                    break;
+                }
+            }
+            System.out.println("");
+            columnas++;
+        }
+    }
+    public static void ej19()
+    {
+        int num = random.nextInt(100 + 1);
+        int intentosRestantes = 10;
+
+        do
+        {
+            System.out.printf("Tienes %d intentos, introduzca el número: ", intentosRestantes);
+            int intento = Integer.parseInt(sc.nextLine());
+            intentosRestantes--;
+
+            if(intento > num)
+            {
+                System.out.println("Has introducido un número mayor al esperado");
+                System.out.println("");
+            }
+            else if(intento < num)
+            {
+                System.out.println("Has introducido un número menor al esperado");
+                System.out.println("");
+            }
+
+            if(intento == num)
+            {
+                System.out.println("¡Enhorabuena has acertado!");
+                System.out.println("");
+                break;
+            }
+
+            if(intentosRestantes == 0)
+            {
+                System.out.println("GAME OVER");
+                System.out.println("");
+                System.out.printf("El número a adivinar era %d\n", num);
+                break;
+            }
+            System.out.println("");
+        }while(intentosRestantes >= 0);
     }
 } 
