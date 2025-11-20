2e1d1943e5207f8e40e42417adb0569a18b93b85
guilleemp26 <guillemporta26@gmail.com>
2025-11-20 01:29:05 +0100

    update

--- UD4/Boletines/Boletin1/Actividad5.java
+++ UD4/Boletines/Boletin1/Actividad5.java
@@ -1,4 +1,3 @@
-
 import java.util.Scanner;
 
 public class Actividad5 
@@ -6,6 +5,81 @@ public class Actividad5
     public static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) 
     {
-        
-    }    
+        System.out.print("Introduzca la cantidad de valores que va a contener la lista: ");
+        int numValores = Integer.parseInt(sc.nextLine());
+
+        double[] lista = leerArray(numValores);
+        double mayor = maximo(lista);
+        double minimo = minimo(lista);
+        double promedio = promedio(lista);
+
+        mostrarResultados(minimo, minimo, promedio);
+    }
+    public static double [] leerArray(int numValores)
+    {
+        double[] lista = new double[numValores];
+
+        for(int i = 0; i < numValores; i++)
+        {
+            System.out.printf("Introduce el valor número %d de la lista: ", i + 1);
+            lista[i] = Double.parseDouble(sc.nextLine());
+        }    
+        return lista;
+    }
+    public static double maximo(double[] lista)
+    {
+        double mayor = 0;
+
+        for(int i = 0; i < lista.length - 1; i++)
+        {
+          if(lista[i] > mayor)
+          {
+            mayor = lista[i];
+          }
+        }
+        return mayor;
+
+    }  
+    public static double minimo(double[] lista)
+    {
+        boolean esMenor = true;
+        double menor = lista[0];
+
+        for(int i = 0; i < lista.length - 1; i++)
+        {
+            for(int j = 0; j < lista.length - 1 && esMenor == true; j++)
+            {
+                if(lista[i] > lista[i + 1])
+                {
+                    esMenor = false;
+                }
+            }
+            if(esMenor == true)
+                {
+                    menor = lista[i];
+                }
+        }
+        return menor;
+    }
+    public static double promedio(double[] lista)
+    {
+        double suma = 0;
+
+        for(int i = 0; i < lista.length; i++)
+        {
+            suma += lista[i];
+        }
+
+        return (suma / lista.length);
+    } 
+    public static void mostrarResultados(double maximo, double minimo, double promedio)
+    {
+        System.out.printf("\n" +
+                         "Mayor número introducido: %.2f\n" +
+                         "Menor número introduciido: %.2f\n" +
+                         "Promedio de los números introducidos: %.2f\n"
+                         , maximo, minimo, promedio    
+                        );
+
+    }
 }
