498191d2e9b28d331afbf86744406dad270f6c84
guilleemp26 <guillemporta26@gmail.com>
2025-11-26 15:22:05 +0100

    Ejercicio 1 del examen terminado

--- 1erEV/Examen1erEV/Ejercicio1.java
+++ 1erEV/Examen1erEV/Ejercicio1.java
@@ -18,6 +18,9 @@ public class Ejercicio1
             case 2:
                 opcion2();
                 break;
+            case 3:
+                opcion3();
+                break;
             default:
                 break;
         }
@@ -107,6 +110,23 @@ public class Ejercicio1
         int tamanyoLista = Integer.parseInt(sc.nextLine());
 
         int[] lista = crearArrayRango(tamanyoLista);
+
+        System.out.print("Introduce un número a buscar el la lista: ");
+        int numIntroducido = Integer.parseInt(sc.nextLine());
+
+        int posNum = buscarPrimerIndice(lista, numIntroducido);
+        boolean posValidada = validarIndice(numIntroducido, lista, posNum);
+        if(!posValidada)
+        {
+            System.out.println("El número introducido no se encuentra en la lista");
+        }
+        else
+        {
+            int indiceNatural = indiceNatural(posNum);
+
+            imprimirResultadosBusqueda(lista, indiceNatural);
+        }
+        
     }
     public static int[] crearArrayRango(int tamanyoLista)
     {
@@ -117,16 +137,11 @@ public class Ejercicio1
         {
             lista[i] = random.nextInt(20 - 10 + 1) + 10;
         }
-        for(int i = 0; i < lista.length; i++)
-        {
-            System.out.println(lista[i]);
-        }
         return lista;
     }
-    public static int buscarPrimerIndice(int[] lista)
+    public static int buscarPrimerIndice(int[] lista, int numIntroducido)
     {
-        System.out.print("Introduce un número a buscar el la lista: ");
-        int numIntroducido = Integer.parseInt(sc.nextLine());
+        
         int posNum = 0;
 
         for(int i = 0; i < lista.length; i++)
@@ -137,5 +152,109 @@ public class Ejercicio1
             }
         }
         return posNum;
+
+    }
+    public static boolean validarIndice(int numIntroducido, int[] lista, int posNum)//Creo un método auxiliar para verificar que el núm introducido está en la posición
+    {
+        if(numIntroducido == lista[posNum])
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    public static int indiceNatural(int posNum)
+    {
+        return ++posNum;
+    }
+    public static void imprimirResultadosBusqueda(int[] lista, int indiceNatural)
+    {
+        for(int i = 0; i < lista.length; i++)
+        {
+            System.out.print(lista[i] + " ");
+        }
+        System.out.println("");
+        System.out.printf("El número que has introducido se encuentra en la posición %d de la lista", indiceNatural);
+    }
+    public static void opcion3()
+    {
+        double areaCirculo = 0;
+        double areaTriangulo = 0;
+        double areaRectangulo = 0;
+
+        mostrarMenuFigura();
+
+        switch (seleccionMenuFigura()) {
+            case 1:
+                System.out.print("Introduce el radio del círculo en cm: ");
+                double radioCirculo = Double.parseDouble(sc.nextLine());
+                areaCirculo = calcularAreaCirculo(radioCirculo);
+                break;
+            case 2:
+                System.out.print("Introduce la base del triángulo en cm: ");
+                double baseTriangulo = Double.parseDouble(sc.nextLine());
+                System.out.print("Introduce la altura del triángulo en cm: ");
+                double alturaTriangulo = Double.parseDouble(sc.nextLine());
+                areaTriangulo = calcularAreaTriangulo(baseTriangulo, alturaTriangulo);
+                break;
+            case 3:
+                System.out.print("Introduce la base del rectángulo en cm: ");
+                double baseRectanculo = Double.parseDouble(sc.nextLine());
+                System.out.print("Introduce la altura del rectángulo en cm: ");
+                double alturaRectanculo = Double.parseDouble(sc.nextLine());
+                areaRectangulo = calcularAreaRectangulo(baseRectanculo, alturaRectanculo);
+            default:
+                break;
+        }
+        double areaSeleccionada = almacenarAreas(areaCirculo, areaTriangulo, areaRectangulo);
+
+        imprimirResultado(areaSeleccionada);
+    }
+    public static void mostrarMenuFigura()
+    {
+        System.out.print("---- Cálculo de áreas ----\n" +
+                        "1. Círculo\n" +
+                        "2. Triángulo\n" +
+                        "3. Rectángulo\n" +
+                        "Seleccione figura:");
+    }
+    public static int seleccionMenuFigura()
+    {
+        System.out.print("Introduce que cálculo quiere realizar: ");
+        return Integer.parseInt(sc.nextLine());
+    }
+    public static double calcularAreaCirculo(double radioCirculo)
+    {
+        return Math.PI * Math.pow(radioCirculo, 2);
+    }
+    public static double calcularAreaTriangulo(double baseTriangulo, double alturaTriangulo)
+    {
+        return (baseTriangulo * alturaTriangulo) / 2;
+    }
+    public static double calcularAreaRectangulo(double baseRectanculo, double alturaRectanculo)
+    {
+        return baseRectanculo * alturaRectanculo;
+    }
+    public static double  almacenarAreas(double areaCirculo, double areaTriangulo, double areaRectangulo)//cambiar void
+    {
+        if(areaCirculo != 0)
+        {
+            return areaCirculo;
+        }
+        else if(areaTriangulo != 0)
+        {
+            return areaTriangulo;
+        }
+        else
+        {
+            return areaRectangulo;
+        }
+    }
+    public static void imprimirResultado(double areaSeleccionada)
+    {
+        System.out.println("");
+        System.out.printf("El área seleccionada es: %.3f en cm", areaSeleccionada);
     }
 }
