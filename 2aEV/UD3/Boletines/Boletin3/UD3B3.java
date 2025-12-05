e7bd0b87c96c76d37ffe4b1e19eff0b3bb22fdab
guilleemo26 <guillemporta26@gmail.com>
2025-12-05 16:43:07 +0100

    updates

--- 2aEV/UD3/Boletines/Boletin3/UD3B3.java
+++ 2aEV/UD3/Boletines/Boletin3/UD3B3.java
@@ -6,7 +6,7 @@ public class UD3B3
     public static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) 
     {
-        ej7();
+        ej12();
     }   
     public static void ej1()
     {
@@ -95,7 +95,69 @@ public class UD3B3
         System.out.print("Introduce una fecha: ");
         String fecha = sc.nextLine();
 
-        Pattern p = Pattern.compile("");
+        Pattern p = Pattern.compile("\\b\\d{2}[/-]?\\d{2}[/-]?\\d{4}\\b");
         Matcher m = p.matcher(fecha);
+
+        while(m.find())
+        {
+            System.out.println("Fechas encontradas: " + m.group());
+        }
+    }
+    public static void ej9()
+    {
+        System.out.print("Ingresa tu correo electrónico: ");
+        String correo = sc.nextLine();
+
+        String patron = "^[A-Za-z\\d]+[@][A-Za-z]+[.][A-Za-z]+$";
+        boolean valido = Pattern.matches(patron, correo);
+
+        System.out.println(valido);
+    }
+    public static void ej10()
+    {
+        System.out.print("Ingresa tu nombre completo: ");
+        String correo = sc.nextLine();
+
+        String patron = "\\b[A-ZÁÉÍÓÚ][a-záéíóúñ]+[ ][A-ZÁÉÍÓÚ][a-záéíóúñ]+[ ][A-ZÁÉÍÓÚ][a-záéíóúñ]+\\b";
+        boolean valido = Pattern.matches(patron, correo);
+
+        System.out.println(valido);
+    }
+    public static void ej11()
+    {
+        System.out.print("Ingresa una frase: ");
+        String frase = sc.nextLine();
+
+        Pattern p = Pattern.compile("([A-Za-z])\1{2,}");
+        Matcher m = p.matcher(frase);
+
+        while(m.find())
+        {
+            System.out.println("Letras tres veces repetidas encontradas: " + m.group());
+        }
+    }
+    public static void ej12()
+    {
+        System.out.print("Ingresa un número hexadecimal: ");
+        String num = sc.nextLine();
+
+        String patron = "^[#][A-Za-z0-9]{6}$";
+        boolean valido = Pattern.matches(patron, num);
+
+        System.out.println(valido);
+    }
+    public static void ej13()
+    {
+        System.out.print("Ingresa una contraseña fuerte: ");
+        String passwd = sc.nextLine();
+
+        String patron = "^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[^A-Za-z0-9]).{8,}$";
+        boolean valido = Pattern.matches(patron, passwd);
+
+        System.out.println(valido);
+    }
+    public static void ej14()
+    {
+        
     }
 }
